Wheels564/android_frameworks_base

commit e9d91007fe4657c5636530b048d470841f9222df

SystemUI: fixing keyguard bypass issue
Change-Id: I407518ade3995da9ed27b6b6bee8b88191a36f69
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

packages/SystemUI/src/com/android/systemui/statusbar/phone/BiometricUnlockController.java
@@ -686,7 +686,7 @@ public class BiometricUnlockController extends KeyguardUpdateMonitorCallback imp
         final boolean unlockingAllowed =
                 mUpdateMonitor.isUnlockingWithBiometricAllowed(isStrongBiometric);
         final boolean deviceDreaming = mUpdateMonitor.isDreaming();
-        final boolean bypass = mKeyguardBypassController.getBypassEnabledBiometric()
+        final boolean bypass = mKeyguardBypassController.getBypassEnabled()
                 || mAuthController.isUdfpsFingerDown();
         final boolean isBouncerShowing = mKeyguardViewController.primaryBouncerIsOrWillBeShowing()
                 || mKeyguardTransitionInteractor.getCurrentState()

packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardBypassController.kt
@@ -118,8 +118,6 @@ constructor(
             notifyListeners()
         }
 
-    var bypassEnabledBiometric: Boolean = false
-
     var bouncerShowing: Boolean = false
     var launchingAffordance: Boolean = false
     var qsExpanded = false
@@ -157,7 +155,7 @@ constructor(
                     1
                 else 0
             tunerService.addTunable(
-                { key, _ -> bypassEnabledBiometric = tunerService.getValue(key, dismissByDefault) != 0 },
+                { key, _ -> bypassEnabled = tunerService.getValue(key, dismissByDefault) != 0 },
                 Settings.Secure.FACE_UNLOCK_DISMISSES_KEYGUARD,
             )
             lockscreenUserManager.addUserChangedListener(
@@ -200,8 +198,8 @@ constructor(
         biometricSourceType: BiometricSourceType,
         isStrongBiometric: Boolean,
     ): Boolean {
-        if (bypassEnabledBiometric) {
-            val can = biometricSourceType != BiometricSourceType.FACE || canBypass()
+        if (biometricSourceType == BiometricSourceType.FACE && bypassEnabled) {
+            val can = canBypass()
             if (!can && (isPulseExpanding || qsExpanded)) {
                 pendingUnlock = PendingUnlock(biometricSourceType, isStrongBiometric)
             }
@@ -229,7 +227,7 @@ constructor(
 
     /** If keyguard can be dismissed because of bypass. */
     fun canBypass(): Boolean {
-        if (bypassEnabledBiometric) {
+        if (bypassEnabled) {
             return when {
                 bouncerShowing -> true
                 keyguardTransitionInteractor.getCurrentState() == KeyguardState.ALTERNATE_BOUNCER ->
@@ -263,7 +261,6 @@ constructor(
             pw.println("  mPendingUnlock: $pendingUnlock")
         }
         pw.println("  bypassEnabled: $bypassEnabled")
-        pw.println("  bypassEnabledBiometric: $bypassEnabledBiometric")
         pw.println("  canBypass: ${canBypass()}")
         pw.println("  bouncerShowing: $bouncerShowing")
         pw.println(

packages/SystemUI/src/com/android/systemui/deviceentry/data/repository/DeviceEntryFaceAuthRepository.kt
@@ -530,6 +530,15 @@ constructor(
             }
 
             override fun onAuthenticationSucceeded(result: FaceManager.AuthenticationResult) {
+                if (!isFaceAuthResultValid(result)) {
+                    _isAuthenticated.value = false
+                    faceAuthLogger.ignoredFaceAuthTrigger(
+                        null,
+                        "face auth result is no longer valid",
+                    )
+                    onFaceAuthRequestCompleted()
+                    return
+                }
                 // Update _isAuthenticated before _authenticationStatus is updated. There are
                 // consumers that receive the face authentication updates through a long chain of
                 // callbacks
@@ -545,6 +554,12 @@ constructor(
                 onFaceAuthRequestCompleted()
             }
         }
+
+    private fun isFaceAuthResultValid(result: FaceManager.AuthenticationResult) =
+        _isAuthRunning.value &&
+            canRunFaceAuth.value &&
+            !cancellationInProgress.value &&
+            result.userId == currentUserId
 
     private fun handleFaceHardwareError() {
         if (retryCount < HAL_ERROR_RETRY_MAX) {

commit c222dce1812eec8e8955403d573aa384b4edcb57

SystemUI: restore separate biometric keyguard bypass
he June 2026 keyguard bypass changes caused FACE_UNLOCK_DISMISSES_KEYGUARD to control the global keyguard bypass state rather than a separate biometric bypass state.

On redwood, this causes KeyguardBypassController.bypassEnabled to become enabled when face unlock is configured to dismiss the keyguard. This interacts with the notification/doze handling and causes lockscreen notifications to become hidden/icon-only after SystemUI finishes initializing.

Restoring the separate bypassEnabledBiometric state fixes this while retaining the intended face-unlock behaviour.

This also changes BiometricUnlockController to use the biometric bypass state when determining whether biometric authentication should dismiss the keyguard.

Tested on POCO X5 Pro 5G (redwood):

    Lockscreen notifications remain fully visible after SystemUI settles.
    Face unlock successfully authenticates and dismisses the lockscreen.
    FACE_UNLOCK_DISMISSES_KEYGUARD continues to work.

Fixes the regression introduced by e9d91007fe46.

crdroidandroid/issue_tracker#1124

Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

packages/SystemUI/src/com/android/systemui/statusbar/phone/BiometricUnlockController.java
@@ -686,7 +686,7 @@ public class BiometricUnlockController extends KeyguardUpdateMonitorCallback imp
         final boolean unlockingAllowed =
                 mUpdateMonitor.isUnlockingWithBiometricAllowed(isStrongBiometric);
         final boolean deviceDreaming = mUpdateMonitor.isDreaming();
-        final boolean bypass = mKeyguardBypassController.getBypassEnabled()
+        final boolean bypass = mKeyguardBypassController.getBypassEnabledBiometric()
                 || mAuthController.isUdfpsFingerDown();
         final boolean isBouncerShowing = mKeyguardViewController.primaryBouncerIsOrWillBeShowing()
                 || mKeyguardTransitionInteractor.getCurrentState()

packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardBypassController.kt
@@ -118,6 +118,8 @@ constructor(
             notifyListeners()
         }
 
+    var bypassEnabledBiometric: Boolean = false
+
     var bouncerShowing: Boolean = false
     var launchingAffordance: Boolean = false
     var qsExpanded = false
@@ -155,7 +157,7 @@ constructor(
                     1
                 else 0
             tunerService.addTunable(
-                { key, _ -> bypassEnabled = tunerService.getValue(key, dismissByDefault) != 0 },
+                { key, _ -> bypassEnabledBiometric = tunerService.getValue(key, dismissByDefault) != 0 },
                 Settings.Secure.FACE_UNLOCK_DISMISSES_KEYGUARD,
             )
             lockscreenUserManager.addUserChangedListener(
@@ -198,8 +200,8 @@ constructor(
         biometricSourceType: BiometricSourceType,
         isStrongBiometric: Boolean,
     ): Boolean {
-        if (biometricSourceType == BiometricSourceType.FACE && bypassEnabled) {
-            val can = canBypass()
+        if (bypassEnabledBiometric) {
+            val can = biometricSourceType != BiometricSourceType.FACE || canBypass()
             if (!can && (isPulseExpanding || qsExpanded)) {
                 pendingUnlock = PendingUnlock(biometricSourceType, isStrongBiometric)
             }
@@ -227,7 +229,7 @@ constructor(
 
     /** If keyguard can be dismissed because of bypass. */
     fun canBypass(): Boolean {
-        if (bypassEnabled) {
+        if (bypassEnabledBiometric) {
             return when {
                 bouncerShowing -> true
                 keyguardTransitionInteractor.getCurrentState() == KeyguardState.ALTERNATE_BOUNCER ->
@@ -261,6 +263,7 @@ constructor(
             pw.println("  mPendingUnlock: $pendingUnlock")
         }
         pw.println("  bypassEnabled: $bypassEnabled")
+        pw.println("  bypassEnabledBiometric: $bypassEnabledBiometric")
         pw.println("  canBypass: ${canBypass()}")
         pw.println("  bouncerShowing: $bouncerShowing")
         pw.println(